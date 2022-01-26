53f3f82d6deb8ed236c936d1aba6a8aed694fb07
Davidedl2001 <davidedeleonardis1711@gmail.com>
2022-01-26 16:53:50 +0100

    add remaining sections, responsive site

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,8 @@
 <template>
     <main>
         <div class="container">
-            <section class="container-intro">
+            <!-- intro -->
+            <section class="container-intro intro">
                 <img src="../assets/img/technology_thumbnail-1-400x300.jpg" alt="img intro main">
 
                 <div class="feature">
@@ -15,7 +16,7 @@
                 </div>
             </section>
 
-            <section class="container-intro order-intro">
+            <section class="container-intro intro order-intro">
                 <div class="feature">
                     <span>
                         <font-awesome-icon :icon="['fab', 'squarespace']" />Investors in people
@@ -29,7 +30,7 @@
                 <img src="../assets/img/construction_thumbnail-400x300.jpg" alt="img intro main">
             </section>
 
-            <section class="container-intro">
+            <section class="container-intro intro">
                 <img src="../assets/img/cryptocurrency-400x300.jpg" alt="img intro main">
 
                 <div class="feature">
@@ -51,7 +52,7 @@
                 <div class="container-brands">
                     <img 
                         v-for="(brand, index) in brands" 
-                        :key="index" 
+                        :key="`A${index}`" 
                         :src="brand.img" 
                         :alt="brand.alt"
                     >
@@ -102,8 +103,8 @@
 
         <!-- characteristics -->
         <div class="bg-gray">
-            <section class="container">
-                <div class="container-intro">
+            <section class="container characteristics">
+                <div class="container-intro order-intro">
                     <div class="feature">
                         <h2>We boost our clients' bottom line by optimizing their growth potential</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quaerat, aliquam a 
@@ -114,24 +115,124 @@
                     <img src="../assets/img/we-boost-our-clients-400x366.jpg" alt="img intro main">
                 </div>
 
-                <div class="middle"></div>
+                <div class="middle">
+                    <div class="col">
+                        <h4>We build relationships</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil officia odit dolore ipsam sapiente quisquam.</p>
+                        <h4>Proprietary process</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil officia odit dolore ipsam sapiente quisquam.</p>
+                    </div>
+                    <div class="col">
+                       <div class="col-left">
+                            <font-awesome-icon :icon="['fas', 'plus-square']" />
+                            <div class="content-middle">
+                                <h5>Accountability</h5>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </div>
+                        <div class="col-left">
+                            <font-awesome-icon :icon="['fas', 'plus-square']" />
+                            <div class="content-middle">
+                                <h5>Transparency</h5>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </div>
+                       <div class="col-left">
+                            <font-awesome-icon :icon="['fas', 'plus-square']" />
+                            <div class="content-middle">
+                                <h5>Investment</h5>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                <div class="awards"></div>
+                <div class="awards">
+                    <span>Awards</span>
+                    <div class="container-awards">
+                        <div 
+                            v-for="(award, index) in awards" 
+                            :key="`B${index}`" 
+                            class="award"
+                        >
+                            <img :src="award.img" :alt="award.alt">
+                            <span>{{ award.text }}</span>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
 
         <!-- employe quote -->
-        <div class="container"></div>
+        <section class="container quote">
+            <div class="container-intro">
+                <img src="../assets/img/large-testimonial-600x583.jpg" alt="img intro main">
+
+                <div class="feature">
+                    <blockquote>"The team at Avada Consultant is faboulus. They helped us unlock our potential
+                        online and offline. We have experienced year on year growth due to theri progressive approach."</blockquote><br>
+                    <span>Kate Schadler</span><br>
+                    <span>Marketing Consulent - Abstract</span>
+                </div>
+            </div>
+        </section>
 
         <!-- resources  -->
         <div class="bg-gray">
-            <section class="container"></section>
+            <section class="container resources">
+                <div class="resources-top">
+                    <h2>Marketing Resouces: <br> Insider Advice on How to Increase Online Sales</h2>
+                    <button>Explore all resources</button>
+                </div>
+
+                <div class="container-cases">
+                    <div class="case">
+                        <img src="../assets/img/blog-posts-6-200x133.jpg" alt="img resource">
+                        <p>Marketing tips nad tricks for your website</p>
+                    </div>
+                    <div class="case">
+                        <img src="../assets/img/blog-posts-5-200x133.jpg" alt="img resource">
+                        <p>How to write stunning blog post titles</p>
+                    </div>
+                    <div class="case">
+                        <img src="../assets/img/blog-posts-4-200x133.jpg" alt="img resource">
+                        <p>Techniques to reduce facebook ads spend</p>
+                    </div>
+                </div>
+            </section>
         </div>
 
         <!-- contact card -->
-        <div class="bg-black">
-            <section class="container"></section>
-        </div>
+        <section class="form">
+            <div class="container">
+                <div class="form-left">
+                    <h1>Let's make things happen</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam maiores aliquam amet, nisi aperiam totam.</p>
+                    <hr>
+                    <blockquote>"The team at Avada Consultant is faboulus. They helped us unlock our potential
+                        online and offline. We have experienced year on year growth due to theri progressive approach."</blockquote>
+                    <span>George Anderson</span><br>
+                    <span>Digitalbox CEO</span>
+                </div>
+
+                <div class="card">
+                    <img src="../assets/img/marketing-consultant-expert.jpg" alt="img Expert jumbo">
+                    <span>Richard Madsen</span>
+                    <span class="card-info">Marketing Consultant Expert</span>
+                    <div>
+                        <font-awesome-icon icon="phone-alt" />
+                        <a href="#">(555) 802-1234</a>
+                    </div>
+                    <form>
+                        <input type="text" placeholder="Your Name*">
+                        <input type="text" placeholder="Your Email*">
+                        <input type="text" placeholder="Your Phone Number*">
+                        <button>Get a Callback</button>
+                    </form>
+                    <span class="card-info">By submitting my data I agree to be contacted</span>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -164,6 +265,28 @@ export default {
                 {
                     "img": require('../assets/img/digitalbox.png'),
                     "alt": "logo Digitalbox"
+                }
+            ],
+            awards: [
+                {
+                    "img": require('../assets/img/award-a-200x191.png'),
+                    "alt": "img award",
+                    "text": "Winner Seo Master MAGT Smart Start Award 2017"
+                },
+                {
+                    "img": require('../assets/img/award-b-200x191.png'),
+                    "alt": "img award",
+                    "text": "Top Social Media Agencies Next Partner 2018"
+                },
+                {
+                    "img": require('../assets/img/award-c-200x191.png'),
+                    "alt": "img award",
+                    "text": "10 Fastest Growing Abstract Solution Providers 2019"
+                },
+                {
+                    "img": require('../assets/img/award-d-200x191.png'),
+                    "alt": "img award",
+                    "text": "National Excellence Agencie Award Winner 020"
                 }
             ]
         }
--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPhoneAlt, faBars, faArrowRight, faFolder, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt, faBars, faArrowRight, faFolder, faUsers, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import { faBandcamp, faSquarespace, faFacebookF, faTwitter, faInstagram, faYoutube, faLinkedinIn, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faPhoneAlt, faBars, faArrowRight, faFolder, faBandcamp, faUsers, faSquarespace, faFacebookF, faTwitter, faInstagram, faYoutube, faLinkedinIn, faTiktok);
+library.add(faPhoneAlt, faBars, faArrowRight, faFolder, faBandcamp, faUsers, faSquarespace, faPlusSquare, faFacebookF, faTwitter, faInstagram, faYoutube, faLinkedinIn, faTiktok);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
